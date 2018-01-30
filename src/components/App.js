43a911dab5a45d5abf4a96c65bdd60c1704d4193
snoop007nj <duonnguyen@yahoo.com>
2018-01-29 21:29:22 -0500

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { addRecipe, removeFromCalendar } from '../actions'
+
+class App extends Component {
+  render() {
+
+    console.log(this.props)
+
+    return (
+      <div>
+        Hello World
+      </div>
+    )
+  }
+}
+
+function mapStateToProps (state) {
+  const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+
+  const calendar  = state
+
+  console.log("calendar", calendar)
+
+  return {
+    calendar: dayOrder.map((day) => ({
+      day,
+      meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+        meals[meal] = calendar[day][meal]
+          ? calendar[day][meal]
+          : null
+
+        return meals
+      }, {})
+    })),
+  }
+}
+
+function mapDispatchToProps (dispatch) {
+  return {
+    selectRecipe: (data) => dispatch(addRecipe(data)),
+    remove: (data) => dispatch(removeFromCalendar(data))
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App)
